9d35792f1cc1dfc6b232ec947415596bb9672f98
Luz <marialuzsabina2001@gmail.com>
2022-05-25 19:30:08 +0200

    Commit de hola luz!

--- ElectricSkate/src/principal/Principal.java
+++ ElectricSkate/src/principal/Principal.java
@@ -5,16 +5,16 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stubç
 		System.out.println("Hola Mundo!");
-		
+
 		System.out.println("Hola de Reginaldo");
-		
+
 		System.out.println("hola alberto");
-		
 
 		System.out.println("Hola de kike");
 
 		System.out.println("Hola jose");
 
+		System.out.println("Hola de luz");
 
 	}
 
