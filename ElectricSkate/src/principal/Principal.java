ef900b7d29ba59caa3d36024f5f818a1b3ad5f5c
Reginaldo <Usuario@LAPTOP-TGG6FT6E>
2022-05-25 18:46:08 +0200

    Commit 2 (Reginaldo)

--- ElectricSkate/src/principal/Principal.java
+++ ElectricSkate/src/principal/Principal.java
@@ -5,6 +5,8 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stubç
 		System.out.println("Hola Mundo!");
+		
+		System.out.println("Hola de Reginaldo");
 
 	}
 
